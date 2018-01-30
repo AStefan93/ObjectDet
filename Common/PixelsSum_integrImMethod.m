27de89aaf0ab37af3e982e810424fab51bb181fd
Sof16 <35914922+Sof16@users.noreply.github.com>
2018-01-30 10:08:59 +0200

    Updated script

--- Common/PixelsSum_integrImMethod.m
+++ Common/PixelsSum_integrImMethod.m
@@ -1,29 +1,32 @@
-function [ integrImg, regionSum] = PixelsSum_integrImMethod(Image, startRow, startCol, endRow, endCol)
-%   The function calculate the integral image of the input image and the sum
-% of pixels content between (startRow,startCol) and (endRow, endCol)
-Image2 = double(Image);
-[Image_height,Image_width] = size(Image2);
+function integrImg = PixelsSum_integrImMethod(Image)
+%   The function calculate the integral image of the input image
 
-row = 1;
-col = 1;
-
-% first column of integrImg
-integrImg(row,col) = Image2(1,1);
-for row = 2:1:Image_height
-    integrImg(row,1) = integrImg(row-1,1) + Image2(row,1);
-end
-
-for col = 2:1:Image_width
-    sum = Image2(1,col);
-    integrImg(1,col) = integrImg(1,col-1) + sum;
-    for row = 2:1:Image_height;
-       sum = sum + Image2(row,col); 
-       integrImg(row,col) = integrImg(row,col-1) + sum;
+if size(Image,3) == 3
+    warndlg('Image must be in grayscale!');
+else
+    Image2 = double(Image);
+    [Image_height,Image_width] = size(Image2);
+    
+    row = 1;
+    col = 1;
+    
+    % first column of integrImg
+    integrImg(row,col) = Image2(1,1);
+    for row = 2:1:Image_height
+        integrImg(row,1) = integrImg(row-1,1) + Image2(row,1);
     end
     
+    % calculation of integral image
+    for col = 2:1:Image_width
+        sum = Image2(1,col);
+        integrImg(1,col) = integrImg(1,col-1) + sum;
+        for row = 2:1:Image_height;
+            sum = sum + Image2(row,col);
+            integrImg(row,col) = integrImg(row,col-1) + sum;
+        end
+        
+    end
 end
-
-regionSum = integrImg(endRow+1,endCol+1) + integrImg(endRow+1,startCol) - integrImg(startRow,endCol+1) - integrImg(startRow,startCol);
 
 end
 
