6ef0975f2930ec5dac7d8e78c028875d792b5e15
Stefan <arnautu.stefan93@gmail.com>
2018-02-12 13:39:15 +0200

    Intermediary step for detection

--- Detector/detector_ferestre_multiple_scalate.m
+++ Detector/detector_ferestre_multiple_scalate.m
@@ -11,17 +11,17 @@ cd(path);
 % if isempty(mypool)
 %     mypool = parpool();
 % end
-load features.mat
+load ../features.mat
 % imag = imread('d:\DSUsers\uidg6179\01_Miscellaneous\10_Matlab\objrec\test_bonsai.jpg');
-imag=imread('D:\ObjectDet-master\test_img\testg.jpg');
+imag=imread('d:\DSUsers\uidg6179\01_Miscellaneous\Git\cut_images\00001\00001_930831_fa_a_test.png');
 %imag=imread('00001_930831_fa_a.ppm');
 %imag=imread('ReadandDisplayImageExample_01.png');
-n = 384;
-m = 256;
+n = 346;
+m = 266;
 flag=1;
 img=rgb2gray(imag);
 [nn,mm]=size(img);
-if (nn < 384 || mm <256)
+if (nn < 346 || mm <266)
     img=imresize(img,[n,m]);
     flag=0;
 end
@@ -45,49 +45,93 @@ end
 % XX5=haar_5(n,m,II,rnd5);
 % incredere = 0;
 raport=nn/n;
-k=0.2;
+k=0.4;
 XX=[];
 off=0;
 j=1;
-while k<raport
-    ls=1;
-    lj=fix(n*k)
-    cs=1;
-    cdr=fix(m*k)
+
+%%testing starts here
+% imshow(img);
+% %II=integralImage(img);
+% ls = length(189:533);
+% cs = length(300:566);
+% imgX=img(189:533,300:566 );
+% II=integralImage(imgX);
+% imshow (imgX);
+% n=ls;
+% m=cs;
+% for i=1:length(featureStrong)
+%     if featureStrong(i).haarFeature.type == 'X1'   
+%         XX(i)=haar_11(n,m,II,featureStrong(i).haarFeature.value);
+%     end
+%     if featureStrong(i).haarFeature.type == 'X2'
+% 
+%         XX(i)=haar_21(n,m,II,featureStrong(i).haarFeature.value);
+%     end
+%     if featureStrong(i).haarFeature.type == 'X3'
+% 
+%         XX(i)=haar_31(n,m,II,featureStrong(i).haarFeature.value);
+%     end
+%     if featureStrong(i).haarFeature.type == 'X4'
+% 
+%         XX(i)=haar_41(n,m,II,featureStrong(i).haarFeature.value);
+%     end
+%     if featureStrong(i).haarFeature.type == 'X5'
+% 
+%         XX(i)=haar_51(n,m,II,featureStrong(i).haarFeature.value);
+%     end
+% end
+% 
+% s_alfah=0;
+% s_alfa=0;
+% for i=1:length(XX)
+%     if XX(i)<featureStrong(i).threshold
+%         s_alfah=s_alfah+featureStrong(i).weight;
+%     end
+%     s_alfa=featureStrong(i).weight+s_alfa;
+% end
+% 
+% if s_alfah > 0.5*s_alfa
+%     fprintf('Evrika!\n',j);
+%     s_alfah
+%     s_alfa/2
+% end
+%%testing ends here
+
+figure()
+imshow (img);
+hold on
+while k<=0.8
+    ls=189;
+    lj=189+fix(n*k)
+    cs=300;
+    cdr=300+fix(m*k)
     
     while off==0
         if flag == 0
-            II=integralImage(img);
+            %II=integralImage(img);
             off=1;
         end
         if flag==1
             imgX=img(ls:lj+1,cs:cdr+1);
-            II=integralImage(img);  
+            II=integralImage(imgX);
+            n=length(ls:lj+1);
+            m=length(cs:cdr+1);
         end
         for i=1:length(featureStrong)
             if featureStrong(i).haarFeature.type == 'X1'
-                n=ls;
-                m=cs;
                 XX(i)=haar_11(n,m,II,featureStrong(i).haarFeature.value);
             end
             if featureStrong(i).haarFeature.type == 'X2'
-                 n=ls;
-                m=cs;
                 XX(i)=haar_21(n,m,II,featureStrong(i).haarFeature.value);
             end
             if featureStrong(i).haarFeature.type == 'X3'
-                 n=ls;
-                m=cs;
                 XX(i)=haar_31(n,m,II,featureStrong(i).haarFeature.value);
             end
             if featureStrong(i).haarFeature.type == 'X4'
-                 n=ls;
-                m=cs;
                 XX(i)=haar_41(n,m,II,featureStrong(i).haarFeature.value);
             end
             if featureStrong(i).haarFeature.type == 'X5'
-                 n=ls;
-                m=cs;
                 XX(i)=haar_51(n,m,II,featureStrong(i).haarFeature.value);
             end
         end
@@ -102,25 +146,31 @@ while k<raport
         
         if s_alfah > 0.5*s_alfa
             fprintf('Evrika %2.0f!\n',j);
-            figure()
-            imshow(imgX)
+            aaa=length(cs:cdr);
+            bbb=length(ls:lj);
+            
+            rectangle ('Position',[cs,ls,aaa,bbb],'EdgeColor','k');
+            pause(0.1)
+           
+%             figure()
+%             imshow(imgX)
         end
-        if cdr+20 < mm
-                cs=cs+20;
-                cdr=cdr+20;
+        if cdr+10 < mm
+                cs=cs+10;
+                cdr=cdr+10;
             else
                 cs=1;
                 cdr=fix(m*k)+1;
-                if lj+20<nn
-                    ls=ls+20;
-                    lj=lj+20;
+                if lj+10<nn
+                    ls=ls+10;
+                    lj=lj+10;
                 else
                     off=1;
                 end
           
             end
-        n = 384;
-        m = 256;
+        n = 346;
+        m = 266;
         j=j+1;
 
     end
