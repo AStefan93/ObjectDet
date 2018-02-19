5fa3c429c3606f66f483cf9f919a10770512e2c1
Stefan <arnautu.stefan93@gmail.com>
2018-02-19 15:37:13 +0200

    Updated detector to use image sizes of 0.1 size

--- Trainer/init.m
+++ Trainer/init.m
@@ -12,10 +12,10 @@ if isempty(mypool)
     mypool = parpool();
 end
 %DataBase Folder
-imgFolderPoz = 'D:\DSUsers\uidg6179\Faces\colorferet\dvd2\gray_feret_cd1\data\images';
-imgFolderNeg = 'D:\DSUsers\uidg6179\Faces\TestFolder\Negatives';
-%imgFolderPoz = 'D:\DSUsers\uidg6179\Faces\negatives\101_ObjRec\bonsai\';
-%imgFolderNeg = 'D:\DSUsers\uidg6179\Faces\negatives\101_ObjRec\other\';
+%imgFolderPoz = 'd:\DSUsers\uidg6179\01_Miscellaneous\10_Matlab\objrec\testing\poz\';
+%imgFolderNeg = 'd:\DSUsers\uidg6179\01_Miscellaneous\10_Matlab\objrec\testing\neg\';
+imgFolderPoz = 'd:\DSUsers\uidg6179\01_Miscellaneous\Git\cut_images\';
+imgFolderNeg = 'd:\DSUsers\uidg6179\01_Miscellaneous\10_Matlab\objrec\101_ObjRec\other\';
 %imgFolderPoz = 'f:\Poze\Image Database\faces\colorferet\dvd1\data\images\';
 %imgFolderNeg = 'f:\Poze\Image Database\faces\negatives';
 %Reading all image locations
@@ -45,29 +45,14 @@ nr_im_total = nr_poz + nr_neg;
 global nr_features_total;
 nr_features_total = 180000;
 nr_features_haar = nr_features_total/5;
-global rnd1 rnd2 rnd3 rnd4 rnd5;
-global rnd_all_struct;
+global rnd1 rnd2 rnd3 rnd4 rnd5 rnd_all;
 rnd1=rand(nr_features_haar,4);
 rnd2=rand(nr_features_haar,4);
 rnd3=rand(nr_features_haar,4);
 rnd4=rand(nr_features_haar,4);
 rnd5=rand(nr_features_haar,4);
-
-rnd1_struct(1:nr_features_total/5) = struct('type','X1','value',0);
-rnd2_struct(1:nr_features_total/5) = struct('type','X2','value',0);
-rnd3_struct(1:nr_features_total/5) = struct('type','X3','value',0);
-rnd4_struct(1:nr_features_total/5) = struct('type','X4','value',0);
-rnd5_struct(1:nr_features_total/5) = struct('type','X5','value',0);
-
-for i= 1:(nr_features_total/5)
-    rnd1_struct(i).value = rnd1(i,:);
-    rnd2_struct(i).value = rnd2(i,:);
-    rnd3_struct(i).value = rnd3(i,:);
-    rnd4_struct(i).value = rnd4(i,:);
-    rnd5_struct(i).value = rnd5(i,:);
-end
-
-rnd_all_struct = [rnd1_struct rnd2_struct rnd3_struct rnd4_struct rnd5_struct];
+%concatenate random vectors
+rnd_all = [rnd1;rnd2;rnd3;rnd4;rnd5];
 
 X1_poz=[];
 X2_poz=[];
@@ -82,11 +67,20 @@ X5_neg=[];
 
 global W_im T;
 %number of iterations (T)
-T = 6000;
+T = 10;
 %initialize weights of images
 W_im(1:nr_im_total) = 1/nr_im_total;
 
+%scale to resize images
+scale = 0.1;
 
 %size of images
-n = 384/10;
-m = 256/10;
+imgInitLocation = imgSetVectorPoz(1).ImageLocation(1);
+img_init = imread(imgInitLocation{1});
+[~, ~, numberOfColorChannels] = size(img_init);
+if numberOfColorChannels > 1
+    % It's a true color RGB image.  We need to convert to gray scale.
+    img_init = rgb2gray(img_init);
+end
+img_init = imresize(img_init,scale);
+[n,m] = size(img_init);
