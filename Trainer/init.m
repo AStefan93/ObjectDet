2b9aa87491bfb85870573007df5ddef297dd97bc
Stefan <arnautu.stefan93@gmail.com>
2018-02-19 15:39:58 +0200

    Updated detector

--- Trainer/init.m
+++ Trainer/init.m
@@ -77,10 +77,8 @@ scale = 0.1;
 %size of images
 imgInitLocation = imgSetVectorPoz(1).ImageLocation(1);
 img_init = imread(imgInitLocation{1});
-[~, ~, numberOfColorChannels] = size(img_init);
-if numberOfColorChannels > 1
-    % It's a true color RGB image.  We need to convert to gray scale.
-    img_init = rgb2gray(img_init);
-end
-img_init = imresize(img_init,scale);
 [n,m] = size(img_init);
+if( n > 100 && m > 100)
+    img_init = imresize(img_init,scale);
+    [n,m] = size(img_init);
+end
